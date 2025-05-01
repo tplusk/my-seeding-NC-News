bcbbd979da0ae5b7e54b8ddf3947810bad6a9cc7
tplusk <macfelelhun@gmail.com>
2025-05-01 14:34:44 +0100

    task 3 is done

--- db/seeds/app.js
+++ db/seeds/app.js
@@ -5,7 +5,7 @@ const { getApi } = require("../../controllers/api.controller");
 const { getTopics } = require("../../controllers/topics.controller");
 const {
   getArticleById,
-  getArticles,
+  /*getArticles,*/
 } = require("../../controllers/articles.controller");
 
 app.get("/api", getApi);
@@ -14,21 +14,28 @@ app.get("/api/topics", getTopics);
 
 app.get("/api/articles/:article_id", getArticleById);
 
-app.get("/api/articles", getArticles);
+// app.get("/api/articles", getArticles);
 
 // error handling
 app.use((err, req, res, next) => {
   console.log(err);
-  //   if (err.status === 400) {
-  res.status(400).send({ msg: "Bad request!" });
-  //   } else next(err);
+  if (err.status === 400) {
+    res.status(400).send({ msg: "Bad request!" });
+  } else next(err);
 });
 
 app.use((err, req, res, next) => {
   console.log(err);
-  //   if (err.status === 404) {
-  res.status(404).send({ msg: "Not found!" });
-  //   } else next(err);
+  if (err.code === "22P02") {
+    res.status(400).send({ msg: "Bad request!" });
+  } else next(err);
+});
+
+app.use((err, req, res, next) => {
+  console.log(err);
+  if (err.status === 404) {
+    res.status(404).send({ msg: "Not found!" });
+  } else next(err);
 });
 
 app.use((err, req, res, next) => {
