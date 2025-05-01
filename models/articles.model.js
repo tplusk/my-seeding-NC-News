f9d887ff90bbe48d6586d68b265dbdeb6e6455dd
tplusk <macfelelhun@gmail.com>
2025-05-01 14:36:35 +0100

    articles.model.js updated

--- models/articles.model.js
+++ models/articles.model.js
@@ -11,13 +11,13 @@ exports.selectArticleById = (articleID) => {
     });
 };
 
-exports.selectArticles = () => {
-  return db
-    .query(
-      `SELECT articles.article_id COUNT (comments.comment_id) AS comment_count FROM articles LEFT JOIN comments ON articles.article_id = comments.article_id GROUP BY articles.article_id ORDER BY articles.article_id`
-    )
-    .then(({ rows }) => {
-      console.log(rows);
-      return rows;
-    });
-};
+// exports.selectArticles = () => {
+//   return db
+//     .query(
+//       `SELECT articles.article_id COUNT (comments.comment_id) AS comment_count FROM articles LEFT JOIN comments ON articles.article_id = comments.article_id GROUP BY articles.article_id ORDER BY articles.article_id`
+//     )
+//     .then(({ rows }) => {
+//       console.log(rows);
+//       return rows;
+//     });
+// };
