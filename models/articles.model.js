8a71164636061a0a2f63a2f7f8d8003138089274
tplusk <macfelelhun@gmail.com>
2025-05-01 14:27:13 +0100

    corrected the articles.model.js, now passes the 404 test

--- models/articles.model.js
+++ models/articles.model.js
@@ -4,6 +4,9 @@ exports.selectArticleById = (articleID) => {
   return db
     .query(`SELECT * FROM articles WHERE article_id = $1`, [articleID])
     .then(({ rows }) => {
+      if (rows.length === 0) {
+        return Promise.reject({ status: 404, msg: "Not found!" });
+      }
       return rows[0];
     });
 };
