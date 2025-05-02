20947956a5386690532275883788ea3b99227e4d
tplusk <macfelelhun@gmail.com>
2025-05-02 10:21:56 +0100

    updated the for the articles, tests are passing

--- controllers/articles.controller.js
+++ controllers/articles.controller.js
@@ -1,19 +1,17 @@
 const {
   selectArticleById,
-  // selectArticles,
+  selectArticles,
 } = require("../models/articles.model");
 
 exports.getArticleById = (req, res, next) => {
   const { article_id } = req.params;
   return selectArticleById(article_id)
-    .then((article) => {
-      res.status(200).send({ article });
-    })
+    .then((article) => res.status(200).send({ article }))
     .catch(next);
 };
 
-// exports.getArticles = (req, res) => {
-//   return selectArticles().then((articles) => {
-//     res.status(200).send({ articles });
-//   });
-// };
+exports.getArticles = (req, res) => {
+  return selectArticles().then((articles) =>
+    res.status(200).send({ articles })
+  );
+};
--- models/articles.model.js
+++ models/articles.model.js
@@ -11,13 +11,24 @@ exports.selectArticleById = (articleID) => {
     });
 };
 
-// exports.selectArticles = () => {
-//   return db
-//     .query(
-//       `SELECT articles.article_id COUNT (comments.comment_id) AS comment_count FROM articles LEFT JOIN comments ON articles.article_id = comments.article_id GROUP BY articles.article_id ORDER BY articles.article_id`
-//     )
-//     .then(({ rows }) => {
-//       console.log(rows);
-//       return rows;
-//     });
-// };
+exports.selectArticles = () => {
+  return db
+    .query(
+      `SELECT 
+        articles.author,
+        articles.title,
+        articles.article_id,
+        articles.topic,
+        articles.created_at,
+        articles.votes,
+        articles.article_img_url,
+        COUNT(comments.comment_id)::INT AS comment_count
+      FROM articles
+      LEFT JOIN comments ON articles.article_id = comments.article_id
+      GROUP BY articles.article_id
+      ORDER BY articles.created_at DESC;`
+    )
+    .then(({ rows }) => {
+      return rows;
+    });
+};
