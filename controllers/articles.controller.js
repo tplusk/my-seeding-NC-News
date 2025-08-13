ddd36257b32e35813a371e67e011ccf05a46a7a5
tplusk <macfelelhun@gmail.com>
2025-08-13 11:45:28 +0100

    add articles topic queries

--- controllers/articles.controller.js
+++ controllers/articles.controller.js
@@ -3,6 +3,7 @@ const {
   selectArticles,
   sendCommentByArticleId,
   sortArticles,
+  sortArticlesByTopic,
 } = require("../models/articles.model");
 
 exports.getArticleById = (req, res, next) => {
@@ -12,24 +13,45 @@ exports.getArticleById = (req, res, next) => {
     .catch(next);
 };
 
-exports.getArticles = (req, res) => {
-  return selectArticles().then((articles) =>
-    res.status(200).send({ articles })
-  );
+exports.getArticles = (req, res, next) => {
+  const { sort_by, order, topic } = req.params;
+  if (topic) {
+    sortArticlesByTopic(topic, sort_by, order)
+      .then((articles) => res.status(200).send({ articles }))
+      .catch(next);
+  } else if (sort_by || order) {
+    sortArticles(sort_by, order)
+      .then((articles) => res.status(200).send({ articles }))
+      .catch(next);
+  } else {
+    selectArticles()
+      .then((articles) => res.status(200).send({ articles }))
+      .catch(next);
+  }
 };
 
 exports.postCommentByArticleId = (req, res, next) => {
-  const { article } = req.params;
-  // const { username, body } = req.body;
+  const { article_id } = req.params;
+  const { username, body } = req.body;
 
-  return sendCommentByArticleId(article, username, body)
+  sendCommentByArticleId(article_id, username, body)
     .then((comment) => {
       res.status(201).send({ comment });
     })
     .catch(next);
 };
 
-exports.getArticlesSortBy = (req, res, next) => {
+exports.getArticlesByCreatedAt = (req, res, next) => {
   const { sort_by = "created_at", order = "desc" } = req.query;
-  res.status(200).send({ msg: `Sorting by ${sort_by} in ${order} order.` });
+  res
+    .status(200)
+    .send({ msg: `Sorting by ${sort_by} in ${order} order.` })
+    .catch(next);
+};
+
+exports.getArticlesByTopic = (req, res, next) => {
+  const { topic } = req.query;
+  return sortArticlesByTopic(topic)
+    .then((articles) => res.status(200).send({ articles }))
+    .catch(next);
 };
